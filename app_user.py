1a2a8965af63ab3d1d6f181656f094290a7019a3
takase-hiroyuki <takase.hiroyuki@gmail.com>
2025-05-25 14:33:02 +0900

    add: トップページ（/）を追加

--- app_user.py
+++ app_user.py
@@ -5,3 +5,14 @@ app = Flask(__name__)
 @app.route("/user/<name>")
 def user_page(name):
     return render_template("user.html", username=name)
+
+@app.route("/")
+def index():
+    return """
+    <h1>Flask User App</h1>
+    <p>次の形式でアクセスしてください：</p>
+    <ul>
+      <li><a href="/user/hiro">/user/hiro</a></li>
+      <li><a href="/user/あなたの名前">/user/あなたの名前</a></li>
+    </ul>
+    """
