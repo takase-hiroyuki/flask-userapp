171fa1d95dc50e742a9eb9b1c326116beef2aad4
takase-hiroyuki <takase.hiroyuki@gmail.com>
2025-05-25 16:25:50 +0900

    fix: ensure gunicorn is included for Render deployment

--- flask_user_app.py
+++ flask_user_app.py
@@ -17,21 +17,6 @@ def index():
     </ul>
     """
 
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return """
-    <h1>Flask User App</h1>
-    <p>次の形式でアクセスしてください：</p>
-    <ul>
-      <li><a href="/user/hiro">/user/hiro</a></li>
-      <li><a href="/user/あなたの名前">/user/あなたの名前</a></li>
-    </ul>
-    """
-
 @app.route("/user/")
 def user_root():
     return """
@@ -41,7 +26,4 @@ def user_root():
       <li><a href="/user/hiro">/user/hiro</a></li>
     </ul>
     """
-
-@app.route("/user/<name>")
-def user_page(name):
-    return render_template("user.html", username=name)
+ 
