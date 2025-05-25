9ee1e3d6dd0b8d28e3ee127e75b4688843245bbf
takase-hiroyuki <takase.hiroyuki@gmail.com>
2025-05-25 14:43:04 +0900

    update: /user/ にアクセスしたときの説明ページを追加

--- app_user.py
+++ app_user.py
@@ -16,3 +16,32 @@ def index():
       <li><a href="/user/あなたの名前">/user/あなたの名前</a></li>
     </ul>
     """
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return """
+    <h1>Flask User App</h1>
+    <p>次の形式でアクセスしてください：</p>
+    <ul>
+      <li><a href="/user/hiro">/user/hiro</a></li>
+      <li><a href="/user/あなたの名前">/user/あなたの名前</a></li>
+    </ul>
+    """
+
+@app.route("/user/")
+def user_root():
+    return """
+    <h2>ユーザー名が指定されていません</h2>
+    <p>次のように URL にユーザー名を追加してください：</p>
+    <ul>
+      <li><a href="/user/hiro">/user/hiro</a></li>
+    </ul>
+    """
+
+@app.route("/user/<name>")
+def user_page(name):
+    return render_template("user.html", username=name)
